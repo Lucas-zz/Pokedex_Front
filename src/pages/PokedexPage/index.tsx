4c9e182ffb5696f92254ace8088f19921d8194ea
Lucas <lucasazzollinivieira@gmail.com>
2022-10-18 19:51:27 -0300

    feat: small pages prototype done

---
+++ src/pages/PokedexPage/index.tsx
@@ -0,0 +1,9 @@
+import { LoadingContainer } from "../style";
+
+export default function ListPage() {
+    return (
+        <LoadingContainer>
+            <h1>This page will be available soon!</h1>
+        </LoadingContainer>
+    );
+}
