ba79f7c661b9fc6ca654357eda42f1381ab6acfb
Lucas <lucasazzollinivieira@gmail.com>
2022-10-18 19:46:36 -0300

    feat: lazy loading implemented and basic app routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CssBaseline } from '@mui/material';
+import './App.css'
+import Alert from './components/Alert';
+import { AlertProvider } from './contexts/alertContext';
+import { GlobalStyle } from './style/reset';
+import Load from "./components/Load";
+import { lazy, Suspense } from "react";
+
+const Loading = () => {
+    return <Load />
+};
+
+const LazyWrapper = (Component: any) => (props: any) => (
+    <Suspense fallback={<Loading />}>
+        <Component {...props} />
+    </Suspense>
+)
+
+const ListPage = LazyWrapper(lazy(() => import('./pages/ListPage')));
+const AchievementsPage = LazyWrapper(lazy(() => import('./pages/AchievementsPage')));
+const PokedexPage = LazyWrapper(lazy(() => import('./pages/PokedexPage')));
+const HelpPage = LazyWrapper(lazy(() => import('./pages/HelpPage')));
+const ConfigPage = LazyWrapper(lazy(() => import('./pages/ConfigPage')));
+
 function App() {
     return (
-        <h1>Hello!</h1>
+        <>
+            <GlobalStyle />
+            <CssBaseline />
+            <AlertProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<ListPage />} />
+                        <Route path='/achievements' element={<AchievementsPage />} />
+                        <Route path='/pokedex' element={<PokedexPage />} />
+                        <Route path='/help' element={<HelpPage />} />
+                        <Route path='/configurations' element={<ConfigPage />} />
+                    </Routes>
+                </BrowserRouter>
+                <Alert />
+            </AlertProvider>
+        </>
     );
 }
 
---
+++ src/components/Load/style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.main`
+    min-width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export {
+    Container
+}
