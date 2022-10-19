866693b3b7278b351222ccfde46da682fc19060c
Lucas <lucasazzollinivieira@gmail.com>
2022-10-18 22:49:19 -0300

    chore: small modification to identify which page is which

--- src/pages/style.tsx
+++ src/pages/style.tsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 
-const LoadingContainer = styled.div`
-    width: 100%;
+const Container = styled.div`
     height: 100vh;
 
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    padding-left: 285px;
+
+    & > :last-child {
+        color: #00A7FD;
+        font-weight: bold;
+    }
 `;
 
 export {
-    LoadingContainer
+    Container
 }
