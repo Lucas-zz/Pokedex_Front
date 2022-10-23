a1a89f2d6948a98cf06e0fd7597ad174596191f8
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2022-10-23 04:52:12 -0300

    feat: filter by maxCP and get min & max CP from the api data implemented

--- src/utils/filterUtils.tsx
+++ src/utils/filterUtils.tsx
@@ -1,5 +1,5 @@
-export function FILTER_BY_TYPE(pokemons: [], filteredTypes: any, setFilteredPokemons: any) {
-    let filteredPokemons: [] = [];
+export function FILTER_BY_TYPE(pokemons: [], filteredTypes: any): [] {
+    let filteredPokemonsByType: [] = [];
     
     const filterByTypeResults = pokemons.map((pokemon: any) => (
         pokemon.types.filter((type: any) => filteredTypes.includes(type))
@@ -7,12 +7,34 @@ export function FILTER_BY_TYPE(pokemons: [], filteredTypes: any, setFilteredPoke
 
     for (let i = 0; i < filterByTypeResults?.length; i++) {
         if (filterByTypeResults[i].length !== 0) {
-            filteredPokemons.push(pokemons[i])
+            filteredPokemonsByType.push(pokemons[i])
         }
     }
-    setFilteredPokemons(filteredPokemons);
+    return filteredPokemonsByType;
 }
 
-export function FILTER_BY_MAX_CP() {
-    return '';
+export function FILTER_BY_MAX_CP(pokemons: any, CPValue: any) {
+    let filteredPokemonsByMaxCP = [];
+
+    for (let i = 0; i < pokemons?.length; i++) {
+        if (pokemons[i].maxCP >= CPValue[0] && pokemons[i].maxCP <= CPValue[1]) {
+            filteredPokemonsByMaxCP.push(pokemons[i])
+        }
+    }
+
+    return filteredPokemonsByMaxCP;
+}
+
+export function GET_MIN_AND_MAX_CP(pokemons: any, setMinMaxCPValue: any) {
+    let minValue = pokemons[0].maxCP;
+    let maxValue = pokemons[0].maxCP;
+    
+    for (let i = 0; i < pokemons.length; i++) {
+        let value = pokemons[i].maxCP;
+
+        minValue = (value < minValue) ? value : minValue;
+        maxValue = (value > maxValue) ? value : maxValue;
+    }
+
+    setMinMaxCPValue([minValue, maxValue]);
 }
