73108d62f8e8bb3e719d21ca1728c2ddccd87cff
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2022-10-23 02:36:03 -0300

    feat: integrating the apollo client in the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import client from './services/api';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+         </ApolloProvider>
     </React.StrictMode>
 );
